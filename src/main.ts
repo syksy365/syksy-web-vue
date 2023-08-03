de5a8b0ef1c95ffbdc095eebb6689ae959a334ab
Dimples_YJ <2890841438@qq.com>
2023-08-03 21:33:54 +0800

    特性(plugins): 增加插件机制安装第三方拓展

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,16 @@
 import './assets/main.css'
 
 import { createApp } from 'vue'
-import { createPinia } from 'pinia'
-import ElementPlus from 'element-plus'
-import 'element-plus/dist/index.css'
 
 import App from './App.vue'
 import router from './router'
 
+import { setupPlugins } from '@/plugins'
+
 const app = createApp(App)
 
-app.use(createPinia())
+setupPlugins(app)
+
 app.use(router)
-app.use(ElementPlus)
 
 app.mount('#app')
